65b807972ff213adf15c52783bc316c25c7b0039
Lewin <junxianalan@gmail.com>
2021-08-10 12:41:25 +0800

    feature: 添加安卓类

--- lib/android/src/main/java/com/lewin/rnopenmapnavigation/RNOpenMapNavigation.java
+++ lib/android/src/main/java/com/lewin/rnopenmapnavigation/RNOpenMapNavigation.java
@@ -1,4 +1,24 @@
 package com.lewin.rnopenmapnavigation;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 
-public class RNOpenMapNavigation {
+public class RNOpenMapNavigation extends ReactContextBaseJavaModule {
+
+    public RNOpenMapNavigation(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "RNOpenMapNavigation";
+    }
+
+    //getMapRouterApp
+    @ReactMethod
+    public void getMapRouterApp(double latitude, double longitude,Promise promise) {
+
+    }
+
 }
